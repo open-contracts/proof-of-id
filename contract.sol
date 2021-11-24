62d0cfdfd09840c555e2afb3becca36a305cbfa0
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-23 18:43:20 -0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,17 +1,26 @@
-pragma solidity ^0.8.3;
-import "@opencontracts/contracts/OpenContractAlpha.sol"
+pragma solidity ^0.8.0;
 
-contract ExampleContract is OpenContractAlpha("e6295f30c2eb24805ed2a1568935a2db73e0d33e26b2a12a84fbad908251b458") {
+import "https://github.com/open-contracts/protocol/blob/main/solidity_contracts/OpenContractRopsten.sol";
+
+contract ProofOfIdentity is OpenContractAlpha {
     
-    string enclaveStorage;
-    string userStorage;
-    
-    function saveFromEnclave (bytes32 oracleHash, string memory someString) public {
-        requireOracle(oracleHash);
-        enclaveStorage = someString;
+    mapping(bytes32 => address) private _account;
+    mapping(address => bytes32) private _ID;
+
+    function getID(address account) public view returns(bytes32) {
+        require(_ID[account] != bytes32(0), "Account never created an ID.");
+        require(_account[_ID[account]] == account, "Account does not have an ID anymore.");
+        return _ID[account];
     }
 
-    function saveFromUser (string memory someString) public {
-        userStorage = someString;
+    function getAccount(bytes32 ID) public view returns(address) {
+        require(_account[ID] != address(0), "ID was never created.");
+        return _account[ID];
+    }
+
+    function createIdentity(bytes32 oracleHash, address msgSender, bytes32 ID) 
+    public _oracle(oracleHash, msgSender, this.createIdentity.selector) {
+        _account[ID] = msgSender;
+        _ID[msgSender] = ID;
     }
 }
