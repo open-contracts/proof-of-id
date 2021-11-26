a4a6da67abdddaf1572a437eab9afafb55875c48
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-26 13:57:49 -0800

    Update oracle.py

--- ssn_oracle/oracle.py
+++ ssn_oracle/oracle.py
@@ -2,6 +2,21 @@ import opencontracts
 from bs4 import BeautifulSoup
 import email, re, os
 
+warning = f"""
+Warning: This is a prototype contract. It allows you to create a unique ID from 
+information that identifies you in the real world, and tie it to your Ethereum account.
+This is cool if you want to get loans without collateral, or take part in
+one-human-one-vote democracy on the blockchain. But here's the bummer: 
+
+Identifying yourself is pretty much at odds with privacy, for now.
+
+Your ID is currently computed from your name ({}), birthday ({}) and last SSN digit ({}).
+By proceeding to create your ID on the blockchain, *it will become public*.
+Anyone who knows (or eventually guesses) these inputs can verify that they produce your ID.
+They will then know this information about you, as well as your current Ethereum account, 
+including all its past and future transactions. Are you *sure* you want to proceed?
+"""
+
 with opencontracts.enclave_backend() as enclave:
 
   def parser(mhtml):
@@ -17,7 +32,8 @@ with opencontracts.enclave_backend() as enclave:
   
   enclave.print(f'Proof of Identity started running in enclave!')
   name, bday, last4ssn = enclave.interactive_session(url='https://secure.ssa.gov/RIL/', parser=parser, instructions=instructions)
-  need_more_unique_data_for_privacy = os.urandom(32) # e.g. get dna data from https://23andme.com?
-  ID = enclave.keccak(name, bday, last4ssn, need_more_unique_data_for_privacy, types=('string', 'string', 'string', 'bytes32'))  
-  enclave.print(f'Confirmed your ID: {ID}')
+  ID = enclave.keccak(name, bday, last4ssn[-1], types=('string', 'string', 'string'))
+  enclave.print(f'Computed your ID: {ID}.')
+
+  enclave.print(warning.format(name, bday, last4ssn[-1]))
   enclave.submit(ID, types=('bytes32',), function_name='createID')
