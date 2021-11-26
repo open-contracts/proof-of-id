e058f68d92c17e4a53968b389a6e7a5a91b61b06
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-26 09:52:26 -0800

    Update oracle.py

--- ssn_oracle/oracle.py
+++ ssn_oracle/oracle.py
@@ -9,8 +9,8 @@ with opencontracts.enclave_backend() as enclave:
     html = [_ for _ in mhtml.walk() if _.get_content_type() == "text/html"][0]
     parsed = BeautifulSoup(html.get_payload(decode=False))
     name = parsed.find(attrs={'id': '3D"uef-container-tabs0"'}).a.text.strip()
-    ssnBox = parsed.find(attrs={'id': '3D"mySSAOverviewSSN"'})
-    ssn, _, bday, _ = ssnBox.findChild().findChild().findChildren()
+    ssn_box = parsed.find(attrs={'id': '3D"mySSAOverviewSSN"'})
+    ssn, _, bday, _ = ssn_box.findChild().findChild().findChildren()
     last4ssn = re.findall('[0-9]{4}', ssn.text.strip())[0]
     bday = bday.text.strip()[14:].strip()
     return name, bday, last4ssn
