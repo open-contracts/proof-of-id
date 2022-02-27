bf91cf297325ddb3a6be03c86837cf28dfa11708
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-26 16:21:24 -0800

    Update oracle.py

--- createID/oracle.py
+++ createID/oracle.py
@@ -2,8 +2,14 @@ import opencontracts
 from bs4 import BeautifulSoup
 import re
 
-with opencontracts.enclave_backend() as enclave:
-  enclave.print(f'Proof of Identity started running in enclave!')
+with opencontracts.session() as session:
+  session.print(f'Proof of ID started running in enclave!')
+  
+  instructions = """
+  1) Log into your account
+  2) Navigate to 'My Profile'
+  3) Click 'Submit'
+  """
   
   def parser(url, html):
     target_url = "https://secure.ssa.gov/myssa/myprofile-ui/main"
@@ -15,23 +21,22 @@ with opencontracts.enclave_backend() as enclave:
       if key.startswith("Date of Birth:"): bday = value.strip()
     return name, bday, last4ssn
   
-  name, bday, last4ssn = enclave.interactive_session(url='https://secure.ssa.gov/RIL/', parser=parser,
-                                                     instructions="Login and visit your SSN account page.")
+  name, bday, last4ssn = session.interactive_browser('https://secure.ssa.gov/RIL/', parser, instructions)
   
   # we divide all 10000 possible last4ssn into 32 random buckets, by using only the last 5=log2(32) bits
   # so last4ssn isn't revealed even if ssn_bucket can be reverse-engineered from ID
-  ssn_bucket = int(enclave.keccak(last4ssn, types=('uint256',))[-1]) % 32
-  ID = enclave.keccak(name, bday, ssn_bucket, types=('string', 'string', 'uint8'))  
+  ssn_bucket = int(session.keccak(last4ssn, types=('uint256',))[-1]) % 32
+  ID = session.keccak(name, bday, ssn_bucket, types=('string', 'string', 'uint8'))  
   
   # publishing your SSN reveals that last4ssn was one of the following possibilites:
   possibilities = list()
-  enclave.expect_delay(8, "Computing ID...")
+  session.expect_delay(8, "Computing ID...")
   for possibility in range(10000):
-    bucket = int(enclave.keccak(possibility, types=("uint256",))[-1]) % 32
+    bucket = int(session.keccak(possibility, types=("uint256",))[-1]) % 32
     if bucket == ssn_bucket: possibilities.append(str(possibility).zfill(4))
   n = len(possibilities)
 
   warning = f'Computed your ID: {"0x" + ID.hex()}, which may reveal your name ({name}), birthday ({bday})'
-  enclave.print(warning + f' and that your last 4 SSN digits are one of the following {n} possibilites: {possibilities}')
+  session.print(warning + f' and that your last 4 SSN digits are one of the following {n} possibilites: {possibilities}')
   
-  enclave.submit(enclave.user(), ID, types=('address', 'bytes32',), function_name='createID')
+  session.submit(session.user(), ID, types=('address', 'bytes32',), function_name='createID')
